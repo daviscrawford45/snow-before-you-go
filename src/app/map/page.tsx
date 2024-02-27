9bfb5bf43d137e76ff9d6c22d8ae40acdeb9e25f
Davis Crawford <daviscrawford45@gmail.com>
2024-02-26 22:48:25 -0700

    Center Map on Utah Resorts

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,41 +1,6 @@
 'use client'
 
-// import * as L from 'leaflet';
 import React, { useEffect, useRef } from 'react';
-
-
-
-// export default function LeafletMap() {
-
-//     var map = L.map('map').setView([51.505, -0.09], 13);
-
-//     return (
-//         <>
-//         </>
-//     );
-// }
-
-
-
-
-
-// function MapComponent() {
-//   const mapRef = useRef(null);
-
-//   useEffect(() => {
-//     if (mapRef.current) {
-//       L.map(mapRef.current).setView([51.505, -0.09], 13);
-//     }
-//   }, []);
-
-//   return <div id="map" ref={mapRef} style={{ height: "100vh", width: "100%" }} />;
-// }
-
-// export default MapComponent;
-
-
-
-
 
 function MapComponent() {
   const mapRef = useRef(null);
@@ -44,7 +9,7 @@ function MapComponent() {
     if (mapRef.current) {
       import('leaflet').then(L => {
         if (mapRef.current) {
-            var map = L.map(mapRef.current).setView([40.763, -111.873], 13);
+            var map = L.map(mapRef.current).setView([40.630, -111.616], 12);
 
             L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
@@ -55,7 +20,7 @@ function MapComponent() {
     }
   }, []);
 
-  return <div id="map" ref={mapRef} style={{ height: "600px", width: "1000px" }} />;
+  return <div id="map" ref={mapRef} />;
 }
 
 export default MapComponent;
